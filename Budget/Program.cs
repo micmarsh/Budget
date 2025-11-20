5c8989e5e6c89642e9a1c43f93f4a492d3be9772
Michael Marsh <mike@marsh.pw>
2025-11-20 16:09:09 -0500

    move unneeded comment

--- Budget/Program.cs
+++ Budget/Program.cs
@@ -8,33 +8,6 @@ using static LanguageExt.Prelude;
 using static Budget.Services.Storage.LiteDB.CustomSerializers;
 using Console = Budget.Console;
 
-//
-//
-// var cats = Seq(new Category("Almsgiving"), new Category("Food"), new Category("Car"));
-//
-// var lineItems = Seq(new LineItem("Frank's POS Charge", 23.34M, DateTime.Now),
-//     new LineItem("Progressive Insurance", 800M, DateTime.Now),
-//     new LineItem("Stuff", 10, DateTime.Now));
-//
-// const string database = "dsfajspdflkjq239r8u9ndsaf.db";
-//
-// // var storage = new LiteDBStorage(database, ObjectId.NewObjectId);
-// //
-// // UserClassification.classifyAll(cats, lineItems)
-// //     .RunUnsafe(new Runtime(default!, storage, new Console()));
-//
-// var mapper = BsonMapper.Global;
-// mapper.RegisterType(serializeSeq<SubCategorized>(mapper), deserializeSeq<SubCategorized>(mapper));
-//
-// using var db = new LiteDatabase(database);
-// var coll = db.GetCollection<ClassificationDoc>(nameof(ClassificationDoc));
-// var catsColl = db.GetCollection<Category>(nameof(Category));
-//
-// var ds = coll.Find(_ => true).ToList();
-// var dbCats = catsColl.Find(_ => true).ToList();
-//
-// System.Console.WriteLine("hello");
-
 // https://stackoverflow.com/a/5721294
 string CreateSHAHash(string phrase)
 {
--- Budget/Services/Storage/LiteDB/LiteDBStorage.cs
+++ Budget/Services/Storage/LiteDB/LiteDBStorage.cs
@@ -63,3 +63,30 @@ public class LiteDBStorage : IStorage
             }),
             Fin: conn => IO.lift(conn.Dispose)).As();
 }
+
+// Basically some repl code
+//
+// var cats = Seq(new Category("Almsgiving"), new Category("Food"), new Category("Car"));
+//
+// var lineItems = Seq(new LineItem("Frank's POS Charge", 23.34M, DateTime.Now),
+//     new LineItem("Progressive Insurance", 800M, DateTime.Now),
+//     new LineItem("Stuff", 10, DateTime.Now));
+//
+// const string database = "dsfajspdflkjq239r8u9ndsaf.db";
+//
+// // var storage = new LiteDBStorage(database, ObjectId.NewObjectId);
+// //
+// // UserClassification.classifyAll(cats, lineItems)
+// //     .RunUnsafe(new Runtime(default!, storage, new Console()));
+//
+// var mapper = BsonMapper.Global;
+// mapper.RegisterType(serializeSeq<SubCategorized>(mapper), deserializeSeq<SubCategorized>(mapper));
+//
+// using var db = new LiteDatabase(database);
+// var coll = db.GetCollection<ClassificationDoc>(nameof(ClassificationDoc));
+// var catsColl = db.GetCollection<Category>(nameof(Category));
+//
+// var ds = coll.Find(_ => true).ToList();
+// var dbCats = catsColl.Find(_ => true).ToList();
+//
+// System.Console.WriteLine("hello");
