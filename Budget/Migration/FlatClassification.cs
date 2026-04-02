afa22953fbee7af41ff6797d810da91f1c781ccf
Michael Marsh <mike@marsh.pw>
2026-04-02 17:01:22 -0400

    need history in csv header

--- Budget/Migration/FlatClassification.cs
+++ Budget/Migration/FlatClassification.cs
@@ -5,7 +5,7 @@ namespace Budget.Migration;
 public readonly record struct FlatClassification(string DbId, DateTime Date, Option<string> Category, string Description, decimal Amount, string History)
 {
     public readonly static string CsvHeader =
-        $"{nameof(DbId)},{nameof(Date)},{nameof(Description)},{nameof(Category)},{nameof(Amount)}";
+        $"{nameof(DbId)},{nameof(Date)},{nameof(Description)},{nameof(Category)},{nameof(Amount)},{nameof(History)}";
     
     public override string ToString() => $"{DbId},\"{Date}\",\"{Description}\",\"{Category.IfNone(string.Empty)}\",{Amount},\"{History}\"";
 }
