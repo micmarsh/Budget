f7253da07567eb9d3c3a6beb6be3a47be7b2fbb9
Michael Marsh <mike@marsh.pw>
2026-04-15 16:11:32 -0400

    reading default configuration

--- Budget/Config/Database.cs
+++ Budget/Config/Database.cs
@@ -1,28 +1,39 @@
-using System.Text.Json;
 
 namespace Budget.Config;
 using LanguageExt;
 using static LanguageExt.Prelude;
 using static LanguageExt.Json<LanguageExt.IO>;
 
+public static class ConfigDefaults
+{
+    public const string DataFile = "budget_config.json";
+    public const string DatabaseFile = "BudgetLiteDb.db";
+    private static readonly string ApplicationData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+    public static readonly string FilePath = Path.Join(ApplicationData, DataFile);
+    public static readonly string DatabasePath = Path.Join(ApplicationData, DatabaseFile);
+
+    public static readonly ConfigData ConfigData = new(DatabasePath);
+}
+
+public readonly record struct ConfigData(string DbLocation); //todo csv parsing info(?) maybe if you want to save it
+
 public static class Database
 {
     //todo move these to non-"Database" class
-    public const string ConfigDirectory = "~/.budget";
-    public const string DataFile = "config";
 
-    public record ConfigData(string DbLocation); //todo csv parsing info(?) maybe if you want to save it
-
-    public static readonly IO<ConfigData> config = IO.lift(() =>
-    {
-        Directory.CreateDirectory(ConfigDirectory);
-        return File.Open(Path.Join(ConfigDirectory, DataFile), FileMode.OpenOrCreate);
-    }).Bind(deserialize<ConfigData>);
+    public static readonly IO<ConfigData> config = +IO.lift(() => File.ReadAllText(ConfigDefaults.DatabaseFile))
+        .Bind(deserialize<ConfigData>)
+        .Catch(e => IO.lift(() =>
+        {
+            System.Console.WriteLine(
+                $"Error reading or parsing config file {ConfigDefaults.FilePath}: '{e.Message}'{Environment.NewLine}" +
+                $"Using default config with databse location {ConfigDefaults.DatabasePath} instead, data may not be read or saved as expected");
+            return ConfigDefaults.ConfigData;
+        }));
 
     // begin database stuff
     
-    public const string DefaultDatabaseFile = "BudgetLiteDb.db";
-
     public static readonly IO<string> readDbFilePath = config.Map(c => c.DbLocation);
 
 }
